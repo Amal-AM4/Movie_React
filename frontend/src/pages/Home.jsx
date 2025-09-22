bcac565b3aeaf3f9eae224083cb74320a872f6f2
Amal A M <vectorhero007@gmail.com>
2025-09-22 12:03:59 +0530

    popularmovie fetching completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,41 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
-import "../css/Home.css"
+import "../css/Home.css";
+import { getPopularMovies, searchMovies } from "../services/api";
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const movies = [
-    { id: 1, title: "The Shawshank Redemption", release_date: "1994-09-23" },
-    { id: 2, title: "The Godfather", release_date: "1972-03-24" },
-    { id: 3, title: "The Dark Knight", release_date: "2008-07-18" },
-    { id: 4, title: "Pulp Fiction", release_date: "1994-10-14" },
-    { id: 5, title: "Forrest Gump", release_date: "1994-07-06" },
-    { id: 6, title: "Inception", release_date: "2010-07-16" },
-    { id: 7, title: "The Matrix", release_date: "1999-03-31" },
-    { id: 8, title: "Goodfellas", release_date: "1990-09-19" },
-    { id: 9, title: "Fight Club", release_date: "1999-10-15" },
-    {
-      id: 10,
-      title: "The Lord of the Rings: The Fellowship of the Ring",
-      release_date: "2001-12-19",
-    },
-    {
-      id: 11,
-      title: "Star Wars: Episode IV - A New Hope",
-      release_date: "1977-05-25",
-    },
-    { id: 12, title: "Léon: The Professional", release_date: "1994-09-14" },
-    { id: 13, title: "Interstellar", release_date: "2014-11-07" },
-    { id: 14, title: "Gladiator", release_date: "2000-05-05" },
-    { id: 15, title: "Whiplash", release_date: "2014-10-10" },
-    { id: 16, title: "The Silence of the Lambs", release_date: "1991-02-14" },
-    { id: 17, title: "Se7en", release_date: "1995-09-22" },
-    { id: 18, title: "Parasite", release_date: "2019-05-30" },
-    { id: 19, title: "The Green Mile", release_date: "1999-12-10" },
-    { id: 20, title: "Spirited Away", release_date: "2001-07-20" },
-    { id: 21, title: "City of God", release_date: "2002-08-30" },
-  ];
+  //   const movies = getPopularMovies()
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -59,14 +49,20 @@ export default function Home() {
         </button>
       </form>
 
-      <div className="movies-grid">
-        {movies.map(
-          (movie) =>
-            movie.title.toLowerCase().startsWith(searchQuery) && (
-              <MovieCard movie={movie} key={movie.id}></MovieCard>
-            )
-        )}
-      </div>
+      {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map(
+            (movie) =>
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id}></MovieCard>
+              )
+          )}
+        </div>
+      )}
     </div>
   );
 }
