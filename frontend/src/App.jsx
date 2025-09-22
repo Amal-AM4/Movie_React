22ec5be9af6d4252672d3e4399835ef45502a50c
Amal A M <vectorhero007@gmail.com>
2025-09-22 11:00:32 +0530

    page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+
 
 function App() {
 
-
   return (
-    <>
-
-    <Home></Home>
-      
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/favorites" element={ <Favorites />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Favorites() {
+  return (
+    <div className='favourites-empty'>
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorites and they will appear here...</p>
+    </div>
+  )
+}
